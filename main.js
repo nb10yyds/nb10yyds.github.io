50f2f44df7872f80e1ce4629156bb7d07417226b
nb10yyds <127301459+nb10yyds@users.noreply.github.com>
2023-09-16 22:04:38 +0800

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-/* 当用户单击图标时，在向 topnav 添加和删除“responsive”响应类之间切换 */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
